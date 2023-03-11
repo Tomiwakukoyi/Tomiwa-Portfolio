cde033371a02a0d9825792f6a19b57e2c95e3ad9
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-03-11 21:53:49 +0100

    Adding my google clone project to the portfolio

--- data/projectsData.js
+++ data/projectsData.js
@@ -516,130 +516,120 @@ export const projectsData = [
       // ],
     },
   },
-  // {
-  //   id: 6,
-  //   title: "Apple New Design System",
-  //   url: "apple-new-design-system",
-  //   category: "Web Application",
-  //   img: "/images/web-project-1.jpg",
-  //   ProjectHeader: {
-  //     title: "Apple New Design System",
-  //     publishDate: "Jul 26, 2021",
-  //     tags: "UI / Frontend",
-  //   },
-  //   ProjectImages: [
-  //     {
-  //       id: uuidv4(),
-  //       title: "Kabul Apple New Design System",
-  //       img: "/images/ui-project-1.jpg",
-  //     },
-  //     {
-  //       id: uuidv4(),
-  //       title: "Kabul Apple New Design System",
-  //       img: "/images/web-project-2.jpg",
-  //     },
-  //     {
-  //       id: uuidv4(),
-  //       title: "Kabul Apple New Design System",
-  //       img: "/images/mobile-project-2.jpg",
-  //     },
-  //   ],
-  //   ProjectInfo: {
-  //     ClientHeading: "About Client",
-  //     CompanyInfo: [
-  //       {
-  //         id: uuidv4(),
-  //         title: "Name",
-  //         details: "Company Ltd",
-  //       },
-  //       {
-  //         id: uuidv4(),
-  //         title: "Services",
-  //         details: "UI Design & Frontend Development",
-  //       },
-  //       {
-  //         id: uuidv4(),
-  //         title: "Website",
-  //         webdetails: "https://company.com",
-  //       },
-  //       {
-  //         id: uuidv4(),
-  //         title: "Phone",
-  //         details: "555 8888 888",
-  //       },
-  //     ],
-  //     ObjectivesHeading: "Objective",
-  //     ObjectivesDetails:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, natus! Quibusdam enim quod in esse, mollitia molestias incidunt quas ipsa accusamus veniam.",
-  //     Technologies: [
-  //       {
-  //         title: "Tools & Technologies",
-  //         techs: [
-  //           "HTML",
-  //           "CSS",
-  //           "JavaScript",
-  //           "Vue.js",
-  //           "TailwindCSS",
-  //           "AdobeXD",
-  //         ],
-  //       },
-  //     ],
-  //     ProjectDetailsHeading: "Challenge",
-  //     ProjectDetails: [
-  //       {
-  //         id: uuidv4(),
-  //         details:
-  //           "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
-  //       },
-  //       {
-  //         id: uuidv4(),
-  //         details:
-  //           "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
-  //       },
-  //       {
-  //         id: uuidv4(),
-  //         details:
-  //           "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
-  //       },
-  //       {
-  //         id: uuidv4(),
-  //         details:
-  //           "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
-  //       },
-  //     ],
-  //     SocialSharingHeading: "Share This",
-  //     // SocialSharing: [
-  //     // 	{
-  //     // 		id: uuidv4(),
-  //     // 		name: 'Twitter',
-  //     // 		icon: <FiTwitter />,
-  //     // 		url: 'https://twitter.com/realstoman',
-  //     // 	},
-  //     // 	{
-  //     // 		id: uuidv4(),
-  //     // 		name: 'Instagram',
-  //     // 		icon: <FiInstagram />,
-  //     // 		url: 'https://instagram.com/realstoman',
-  //     // 	},
-  //     // 	{
-  //     // 		id: uuidv4(),
-  //     // 		name: 'Facebook',
-  //     // 		icon: <FiFacebook />,
-  //     // 		url: 'https://facebook.com/',
-  //     // 	},
-  //     // 	{
-  //     // 		id: uuidv4(),
-  //     // 		name: 'LinkedIn',
-  //     // 		icon: <FiLinkedin />,
-  //     // 		url: 'https://linkedin.com/',
-  //     // 	},
-  //     // 	{
-  //     // 		id: uuidv4(),
-  //     // 		name: 'Youtube',
-  //     // 		icon: <FiYoutube />,
-  //     // 		url: 'https://www.youtube.com/c/StomanStudio',
-  //     // 	},
-  //     // ],
-  //   },
-  // },
+  {
+    id: 6,
+    title: "Google Clone",
+    url: "Google-clone",
+    category: "Web Application",
+    img: "/images/google.png",
+    ProjectHeader: {
+      title: "Google Clone App",
+      publishDate: "Mar, 2023",
+      tags: "UI / Frontend",
+    },
+    ProjectImages: [
+      {
+        id: uuidv4(),
+        title: "Google",
+        img: "/images/google.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Google",
+        img: "/images/google1.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Google",
+        img: "/images/google2.png",
+      },
+    ],
+    ProjectInfo: {
+      ClientHeading: "About Client",
+      CompanyInfo: [
+        {
+          id: uuidv4(),
+          title: "Name",
+          details: "Personal Project",
+        },
+        {
+          id: uuidv4(),
+          title: "Services",
+          details: "UI Design & Frontend Development",
+        },
+        {
+          id: uuidv4(),
+          title: "Website",
+          webdetails: "https://googleclone-jade.vercel.app/",
+        },
+      ],
+      ObjectivesHeading: "Objective",
+      ObjectivesDetails:
+        "The plan was to make a web app that looked almost exactly and performed like Google using React JS.",
+      Technologies: [
+        {
+          title: "Tools & Technologies",
+          techs: [
+            "React JS",
+            "CSS",
+            "Context API",
+            "React-Router",
+            "Google Custom Search API",
+            "Material UI",
+          ],
+        },
+      ],
+      ProjectDetailsHeading: "Challenge",
+      ProjectDetails: [
+        {
+          id: uuidv4(),
+          details:
+            "Building the UI of this web app wasn't that difficult as I am familiar with the Google UI already. I basically just replicated the Homepage quite easily.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "A challenging part on this project was when I was working with the context Api. It was really tricky but I think I'm getting a hang of it already. Calling the API from google was pretty straightforward.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Another intersting aspect of this project was when I had to create my own custom hook for calling the API data. Custom hooks are really great!",
+        },
+      ],
+      // SocialSharingHeading: "Share This",
+      // SocialSharing: [
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Twitter',
+      // 		icon: <FiTwitter />,
+      // 		url: 'https://twitter.com/realstoman',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Instagram',
+      // 		icon: <FiInstagram />,
+      // 		url: 'https://instagram.com/realstoman',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Facebook',
+      // 		icon: <FiFacebook />,
+      // 		url: 'https://facebook.com/',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'LinkedIn',
+      // 		icon: <FiLinkedin />,
+      // 		url: 'https://linkedin.com/',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Youtube',
+      // 		icon: <FiYoutube />,
+      // 		url: 'https://www.youtube.com/c/StomanStudio',
+      // 	},
+      // ],
+    },
+  },
 ];
