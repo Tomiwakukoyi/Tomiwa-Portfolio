673bed59b7f8c14cfd0a8e65f90cc4c77f976a0f
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-03-22 02:27:07 +0100

    Fixing cv issue

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -50,7 +50,7 @@ function AppBanner() {
         >
           <a
             download="Tomiwa Kukoyi Frontend Resume.pdf"
-            href="/files/tomiwakukoyicv.pdf"
+            href="/files/tomiwakukoyi.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
           >
