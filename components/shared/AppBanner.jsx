b1517aeec1d2dd5034767f349cd7943b4d0267db
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2024-01-04 19:56:11 +0100

    change to the app banner

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -34,9 +34,10 @@ function AppBanner() {
             duration: 0.9,
             delay: 0.2,
           }}
-          className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
+          className="font-general-medium mt-4 text-md md:text-lg lg:text-lg  text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
         >
-          A Website Developer & ICT Expert
+          Full Stack Developer Specialized in Frontend Development with stacks;
+          React.js / Next.js, Javscript / Typescript, AWS, Tailwind CSS.
         </motion.p>
         <motion.div
           initial={{ opacity: 0 }}
